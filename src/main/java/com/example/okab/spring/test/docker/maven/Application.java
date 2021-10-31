3e44f180fc74ad21e16baf76cea7ccc1a664e034
eliot andelon <eliotandelon@gmail.com>
2021-10-30 22:30:19 -0500

    first commit

---
+++ src/main/java/com/example/okab/spring/test/docker/maven/Application.java
@@ -0,0 +1,13 @@
+package com.example.okab.spring.test.docker.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/okab/spring/test/docker/maven/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okab.spring.test.docker.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
